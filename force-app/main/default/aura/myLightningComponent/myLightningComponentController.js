030f706b4089d838db3412316c945b41a792ea38
Fabien Taillon <ftaillon@gmail.com>
2017-07-19 13:54:48 +0200

    Adding linting error that can be deployed in an org

--- force-app/main/default/aura/myLightningComponent/myLightningComponentController.js
+++ force-app/main/default/aura/myLightningComponent/myLightningComponentController.js
@@ -1,5 +1,10 @@
 ({
     doInit : function(component, event, helper) {
         component.set('v.setMeOnInit', 'Hello World');
-    
+
+        var test = {
+            'FirstName' : 'Fabien'
+            'LastName'  : 'Taillon'
+        }
+    }
 })
